e06d9a404f2d396f90c1add3bf90e7f318c8e5ca
Lucas-megaleios <lucas-unifei@outlook.com>
2021-08-01 22:52:49 -0300

    Passing params

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,10 +1,43 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function Contacts() {
+export default function Contacts({ navigation }) {
   return (
     <View>
-      <Text>Contatos</Text>
+      <View style={{ margin: 20 }}>
+        <Text>Nome: Lucas Santos</Text>
+        <Text>Telefone: (77) 77777-7777</Text>
+        <Text
+          onPress={() =>
+            navigation.navigate("Information", {
+              nome: "Lucas Santos",
+              telefone: "(77) 7777-7777",
+              endereco: "Rua 777",
+              numero: "777",
+              profissao: "Desenvolvedor mobile",
+            })
+          }
+        >
+          Information...
+        </Text>
+      </View>
+      <View style={{ margin: 20 }}>
+        <Text>Nome: Lucas Gomes</Text>
+        <Text>Telefone: (11) 11111-1111</Text>
+        <Text
+          onPress={() =>
+            navigation.navigate("Information", {
+              nome: "Lucas Gomes",
+              telefone: "(11) 11111-1111",
+              endereco: "Rua 111",
+              numero: "111",
+              profissao: "Desenvolvedor mobile",
+            })
+          }
+        >
+          Information...
+        </Text>
+      </View>
     </View>
   );
 }
--- src/pages/Information/index.js
+++ src/pages/Information/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function Information() {
+export default function Information({ route }) {
   return (
-    <View>
-      <Text>Information</Text>
+    <View style={{ margin: 20 }}>
+      <Text>Nome: {route.params?.nome}</Text>
+      <Text>Telefone: {route.params?.telefone}</Text>
+      <Text>Endereço: {route.params?.endereco}</Text>
+      <Text>N°: {route.params?.numero}</Text>
+      <Text>Profissão: {route.params?.profissao}</Text>
     </View>
   );
 }
